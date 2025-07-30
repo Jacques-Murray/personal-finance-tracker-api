ed543d6ec1257953a18529d4db511d7c63b618f9
Jacques Murray <jacquesmmurray@gmail.com>
2025-07-30 08:45:06 +0000

    feat: add validation to CreateCategory and update Category model

--- internal/models/category.go
+++ internal/models/category.go
@@ -5,7 +5,7 @@ import "time"
 // Category represents a classification for a transaction
 type Category struct {
 	ID        uint      `gorm:"primaryKey" json:"id"`
-	Name      string    `gorm:"size:100;not null;unique" json:"name"`
+	Name      string    `gorm:"size:100;not null;unique" json:"name" validate:"required,min=2,max=100"`
 	ParentID  *uint     `json:"parentId,omitempty"`
 	Parent    *Category `gorm:"foreignKey:ParentID" json:"parent,omitempty"`
 	UserID    uint      `json:"userId"`
